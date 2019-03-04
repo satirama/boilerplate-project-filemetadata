2d17476a2751d8661edc0a792246eed4d987a24e
satirama <mirandas.angelica@gmail.com>
2019-03-03 19:02:22 -0600

    Add fileanalyse endpoint

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 var express = require('express');
 var cors = require('cors');
+var url = "mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@ds139985.mlab.com:39985/fcc-filemetadata";
 
 // require and use "multer"...
-
+var multer = require('multer');
+var upload = multer();
 var app = express();
 
 app.use(cors());
@@ -20,4 +22,11 @@ app.get('/hello', function(req, res){
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
+});
+
+app.post("/api/fileanalyse",upload.single('upfile'),function(req, res){
+  res.json({
+    fileName: req.file.originalname,
+    fileSize: req.file.size/1000 + 'KB'
+  });
 });
